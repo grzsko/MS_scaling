47005225263ed3921a237f1718d8b229ef92f9f9
Grzegorz Skoraczynski <g.skoraczynski@mimuw.edu.pl>
2021-09-14 17:44:22 +0000

    Codebase moved

---
+++ MassSinkhornmetry/optimal_transport_dense.h
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  optimal_transport.h
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  11/04/19 10:54:46
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Grzegorz Skoraczynski
+ *   Organization:  MIMUW
+ *
+ * =====================================================================================
+ */
+
+#include <Eigen/Dense>
+#include <tuple>
+#include <vector>
+
+#ifndef optimal_transport_dense_h__
+#define optimal_transport_dense_h__
+double calc_distance_dense(std::vector<double> &, std::vector<double> &,
+                           std::vector<double> &, std::vector<double> &, double,
+                           double, double, double, int, const std::string);
+
+std::vector<std::tuple<int, int, double>> calc_steps_dense(
+    std::vector<double> &, std::vector<double> &, std::vector<double> &,
+    std::vector<double> &, double, double, double, double, int,
+    const std::string);
+
+double calc_distance_dense(std::vector<double> &, std::vector<double> &,
+                           Eigen::MatrixXd, double, double, double, double, int,
+                           const std::string);
+
+std::vector<std::tuple<int, int, double>> calc_steps_dense(
+    std::vector<double> &, std::vector<double> &, Eigen::MatrixXd, double,
+    double, double, double, int, const std::string);
+#endif
